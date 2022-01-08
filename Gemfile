8ba2d7ee026fe21d9b7347312f1a2d17da7eda27
Gary Yau <gary.yau@hotmail.com>
2022-01-08 14:10:40 +0900

    basic completion

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'font-awesome-sass', '~> 5.15.1'
 
+gem 'simple_form'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  # show all
+  get '/tasks', to: 'tasks#index', as: :tasks
+   # create a task
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # update a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # show one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  patch '/tasks/:id/', to: 'tasks#update'
+  # destroy a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
